bcc4db5f1693e7e68b0569d710e0c7f39da67f5a
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-11-13 20:49:27 +0700

    first Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './App/Pages/MainPage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
