767e001e9d3e211fc7493c8c4427e52910df9d79
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-11-13 22:54:46 +0700

    Working on the InfoBox

--- src/App/Components/InfoBox/InfoBox.jsx
+++ src/App/Components/InfoBox/InfoBox.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
 
-export default function InfoBox() {
+
+
+
+export default function InfoBox({title, cases, total}) {
     return (
-        <div>
-            
-        </div>
+        <Card className='infoBox__main'>
+            <CardContent>
+                <Typography className='infoBox__subTitle' color='textSecondary'>
+                    {title}
+                </Typography>
+                <h2 className='infoBox__title'>{cases}</h2>
+                <Typography className='infoBox__subTitle' color='textSecondary'>
+                    {total}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
--- src/App/Pages/MainPage/MainPage.jsx
+++ src/App/Pages/MainPage/MainPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import InfoBox from '../../Components/InfoBox/InfoBox'
+
+import './StyleMainPage.css'
 
 export default function MainPage() {
     return (
         <div>
             <Header />
+            <div className='mainPage__infoBox'>
+                <InfoBox title='Corovirse Case' total={20200200} cases={939939}/>
+                <InfoBox title='recovered ' total={10033} cases={939939}/>
+                <InfoBox title='Deaths' total={20200200} cases={30300}/>
+            </div>
         </div>
     )
 }
