f131709d9d1351e3a9f61422b7f8aadb1856a20a
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-11-14 11:56:25 +0700

    Fixing the Design and start working on the graph

--- src/App/Components/InfoBox/InfoBox.jsx
+++ src/App/Components/InfoBox/InfoBox.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
+import './StyleInfoBox.css'
+
 
 
 
 
 
 export default function InfoBox({title, cases, SubTitle, total}) {
+ 
     return (
         <Card className='infoBox__main'>
             <CardContent>
-                <Typography className='infoBox__subTitle' color='textSecondary'>
-                    {title}
+                <h2 className='infoBox__subTitle' >
+                    {title} 
+                </h2>
+                <Typography className='infoBox__title'> 
+                   total:  {cases} 
                 </Typography>
-                <h2 className='infoBox__title'>Total: {cases}</h2>
                 <Typography className='infoBox__subTitle' color='textSecondary'>
-                   today {SubTitle} {total}
+                   today {SubTitle}  {total }
                 </Typography>
             </CardContent>
         </Card>
